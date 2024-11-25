9116e2a06ccd94cb5612f44f08a99b53475eef96
taepsteinnelson <taepsteinnelson@my.waketech.edu>
2024-11-25 13:28:11 -0500

    Main Menu Changes(not finished)

--- RacingGame/Assets/Scripts/UI Code/MenuController.cs
+++ RacingGame/Assets/Scripts/UI Code/MenuController.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Cinemachine;
+
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private GameObject helpPanel, creditsPanel, leftButton, rightButton;
     [SerializeField] private TMP_Text helpTitle, helpText;
+    public CinemachineDollyCart dollyCart;
+    public CinemachinePath initialPath;  // Side view path
+    public CinemachinePath mainPath;     // Track path after selecting a car
+    public float speed = 5f;
+
+    private bool movingForward = false;
+    private bool movingBackward = false;
     private Scene scene;
     void Start()
     {
@@ -19,16 +28,42 @@ public class MenuController : MonoBehaviour
             creditsPanel.gameObject.SetActive(false);
             leftButton.gameObject.SetActive(false);
         }
+
+        // Set initial path to the side view
+        dollyCart.m_Path = initialPath;
+        dollyCart.m_Position = 0f;
+        dollyCart.m_Speed = 0f;  // Start with speed 0 to keep it stationary
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Stop the camera when it reaches the end of the path (either forward or backward)
+        if (movingForward && dollyCart.m_Position >= dollyCart.m_Path.PathLength)
+        {
+            dollyCart.m_Speed = 0f;
+            movingForward = false;
+        }
+        else if (movingBackward && dollyCart.m_Position <= 0f)
+        {
+            dollyCart.m_Speed = 0f;
+            movingBackward = false;
+        }
     }
     public void OnPlayButtonClicked()
     {
-        SceneManager.LoadScene(scene.buildIndex + 1);
+        dollyCart.m_Path = mainPath;
+        movingForward = true;
+        movingBackward = false;
+        dollyCart.m_Speed = speed;  // Set speed to move forward
+       // SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+    public void OnBackToMainMenu()
+    {
+        dollyCart.m_Path = initialPath;
+        movingForward = false;
+        movingBackward = true;
+        dollyCart.m_Speed = -speed;  // Set speed to move backward
     }
     public void OnHelpButtonClicked()
     {
